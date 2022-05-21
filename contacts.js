68b59dc55b168cc6a1fdd2b0894aa245095393ec
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2022-05-21 19:45:27 +0300

    добавляет 2, 3 шаг дз

--- contacts.js
+++ contacts.js
@@ -5,23 +5,41 @@ const uuid = require("uuid")
 const contactsPath = path.join(__dirname, 'db/contacts.json');
 
 
-// TODO: задокументировать каждую функцию
 async function listContacts() {
-    const dataString = await fs.readFile(contactsPath);
-    const data = JSON.parse(dataString);
-    return data
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find(constant => constant.id === contactId);
+  return contact ? contact : null;
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  const newContact = {
+    id: uuid.v4(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+
+  const removeContact = contacts[index];
+  if(index !== -1) {
+    contacts.splice(index,1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  }
+  return removeContact ? removeContact : null;
 }
 
-module.exports = { listContacts };
+module.exports = { listContacts, getContactById, addContact, removeContact };
--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const contactsOperations = require('./contacts');
 
-const invokeAction = async({ action, id, data }) => {
+async function invokeAction({ action, id, name, email, phone }){
     switch(action) {
-        case "listContacts":
-            const contacts = await contactsOperations.listContacts();
+        case "list":
+            const contacts = await contactsOperations.list();
             console.log(contacts);
             break;
+        case "get":
+            const contact = await contactsOperations.get(id);
+            if (!contact) {
+                throw new Error(`Product with id=${id} not found`);
+            };
+            console.log(contact);
+            break;
+        case "add":
+            await contactsOperations.add(name, email, phone);
+            break;
+        case "remove":
+            await contactsOperations.removeContact(id);
+            break;
+        default:
+            console.log("Unknown action")
     }
 }
 
-invokeAction({action:'listContacts'})
+// invokeAction({ action: 'listContacts' })
+
+// invokeAction({action:'getContactById', id: '5' })
+
+// invokeAction({
+//     action: 'addContact',
+//     name: 'John Rambo',
+//     email: "johnrambo@mail.com",
+//     phone: "(826) 145-8756"
+// })
+
+invokeAction({action: 'remove', id: 'bb8b7834-9cc5-4dc0-a1f3-501f15888b8c'})
+
+
